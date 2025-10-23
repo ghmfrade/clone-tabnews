73b27a257b20e3eddd7e55363401f201b9f690ad
Guilherme Frade <ghm_frade@hotmail.com>
2025-10-23 00:44:57 -0300

    Configura api status

--- pages/api/status.js
+++ pages/api/status.js
@@ -1,5 +1,7 @@
 function status(request, response) {
   response.status(200).json({
-    chave: "são acima da média",
+    alunos_curso_dev: "são acima da média",
   });
 }
+
+export default status;
