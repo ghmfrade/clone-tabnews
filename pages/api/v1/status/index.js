c63ac07c6e087fbe3ffd6679f0f16b4bbfeccc06
Guilherme Frade <ghm_frade@hotmail.com>
2025-10-30 23:14:23 -0300

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const vDatabase = await database.query("SHOW server_version;");
+  const versionDatabase = vDatabase.rows[0].server_version;
+
+  const maxConn = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(maxConn.rows[0].max_connections);
+
+  const activeConn = await database.query({
+    text: `SELECT COUNT(*)::int AS number_of_active_connections
+           FROM pg_stat_activity
+           WHERE datname = $1`,
+    values: [process.env.POSTGRES_DB],
+  });
+  const activeConnections = activeConn.rows[0].number_of_active_connections;
+
   response.status(200).json({
-    alunos_curso_dev: "são acima da média",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version_database: versionDatabase,
+        max_connections: maxConnections,
+        active_connections: activeConnections,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("GET api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  // Testa status da api
   expect(response.status).toBe(200);
+
+  // Testa data no servidor
+  parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+
+  // Testa versão do banco de dados
+  expect(responseBody.dependencies.database.version_database).toEqual("16.0");
+
+  // Testa maximo de conexões
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+
+  // Testa conexões ativas
+  expect(responseBody.dependencies.database.active_connections).toBe(1);
 });
